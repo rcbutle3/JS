2efcd2102bf726279ec1efeeab17b981c2e8e661
Robin Butler <Robin@Robins-MacBook-Air.local>
2017-07-25 15:52:14 -0400

    js

---
+++ main.js
@@ -0,0 +1,227 @@
+// Answer the following questions in this file using comments without running the code
+
+// 1.
+// What does `givenName` equal right now?
+let givenName;
+// A:undefined
+
+
+// 2.
+// What is `givenName` set to right now?
+givenName = "Robin";
+// A:Tim
+
+
+// 3.
+// What is `givenName` set to right now?
+givenName = givenName;
+// A:givenName
+
+
+// 4.
+// What is `greeting` set to?
+let greeting = "Hello, how are you? " + Robin;
+// A: Hello, how are you? Tim
+
+
+// 5.
+// What is `math` set to?
+let high = 50;
+let low  = 10;
+let math = high - low;
+// A: 40
+
+
+// 6.
+// What is `math` set to?
+math = 50 - "5";
+// A:45
+
+
+// 7.
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 33 years old"
+// The answer shoud not be written in a comment.
+let born = 1984;
+let today = 2017;
+// A:2017-1984
+  let (T-B)= "V"
+
+
+// 8.
+// Adjust this code. Store some information in the following variables.
+// A:Your Name= Robin
+     instructorName= Kelley
+let "Robin";
+let "Kelley";
+
+
+// 9.
+// Update the variables `yourName` and `instructorName` so the expression below reads correctly.
+// A: let statement= Robin + is taking a class at The Iron Yard, my instructors name is Kelley
+
+
+// This statement should read correctly
+let statement = "Robin" + " is taking a class at The Iron Yard, my instructor's name is " + "Kelley" ;
+
+
+// 10.
+// We've created a variable named after each primitive type.
+// Use the `=` operator to assign a value to each variable matching the type in the name.
+let='33';
+let "Robin";
+let "true";
+// This one's a little tricky - think carefully about what makes a value "undefined"!
+let " " ;
+// A:myUndefined
+
+
+// For questions 11 - 19: List which boolean value will be stored in each variable. (Remember: `==` does not check data type)
+
+// 11.
+let x = ("false");
+// A. true
+
+
+// 12.
+let y = ("true");
+// A.true
+
+
+// 13.
+let z = ("false");
+// A.
+
+
+// 14.
+let a = ("true");
+// A.
+
+
+// 25.
+let b = ("true");
+// A.
+
+
+// 16.
+let c = ("false");
+// A.
+
+
+// 17.
+let d = ("true");
+// A.
+
+
+// 18.
+let e = ("false");
+// A.
+
+
+// 19.
+let f = ("true");
+// A.
+
+
+// 20.
+// Fix the `if...else` statement below by adding an `if` keyword and conditional expression.
+// Use the variable `thirsty` as part of your conditional expression.
+// A:
+let thirsty = true;
+
+{
+  console.log("I'm parched!");
+} else {
+  console.log("I've had enough to drink.");
+}
+
+// For questions 21 - 29 list which statement will be logged.
+
+// 21.2
+let x;
+
+if(x){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 22.
+if(""){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 23.
+// this expression will set x to NaN
+let x = 1 * undefined;
+
+if(x){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 24.
+if(false){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 25.
+if(-5 && 5){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 26.
+if("Hello World!"){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 27.
+if({}){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 28.
+if({name: "Yehuda Katz"}){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 29.
+if(new Date()){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:
+
+
+// 30.
+// Link this file to the sibling `index.html` document, open the file in your browser and make sure there are no errors in your console.
